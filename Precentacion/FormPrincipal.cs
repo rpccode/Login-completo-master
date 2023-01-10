c0f819a02dd453fd3fa6be4198950dcb6a0ee2a1
Rudy <rpccode@gmail.com>
2023-01-10 19:32:21 -0400

    v

---
+++ Precentacion/FormPrincipal.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Common.Cache;
+
+
+namespace Presentation
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        #region
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void botoncerrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que quiere cerrar la Aplicaccion", "warnig", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnmax.Visible = false;
+            btnrest.Visible = true;
+        }
+
+        private void btnrest_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnrest.Visible = false;
+            btnmax.Visible = true;
+        }
+
+        private void btnmin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+           
+
+        }
+        
+        private void abrirmihija(object formhija)
+        {
+            if(this.panelcontenedor.Controls.Count>0)
+            {
+                this.panelcontenedor.Controls.RemoveAt(0);
+            }
+            Form fh = formhija as Form;
+            fh.TopLevel = false;
+            fh.FormBorderStyle = FormBorderStyle.None;
+            fh.Dock = DockStyle.Fill;
+
+            this.panelcontenedor.Controls.Add(fh);
+            this.panelcontenedor.Tag = fh;
+            fh.Show();
+        }
+
+        private void btnProducto_Click(object sender, EventArgs e)
+        {
+            abrirmihija(new productos()); 
+        }
+
+        private void btninicio_Click(object sender, EventArgs e)
+        {
+            abrirmihija(new Inicio());
+        }
+
+        private void barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void LoadUserData()
+        {
+            lblName.Text = UserLoginCache.FirsName+", "+ UserLoginCache.LastName;
+            lblPosition.Text = UserLoginCache.Position;
+            lblEmail.Text = UserLoginCache.Email;
+        }
+
+
+
+#endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btninicio_Click(null, e);
+            LoadUserData();
+            //permisos de usuarios
+            if(UserLoginCache.Position == Cargos.Accounting)
+            {
+                btnclientes.Enabled = false;
+                btnProducto.Enabled = false;
+                btnventas.Enabled = false;
+
+
+
+            }
+            if(UserLoginCache.Position == Cargos.Receptionist)
+            {
+                btncompras.Enabled = false;
+                btnEmpleados.Enabled = false;
+                
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea cerrar la secion", "warnig", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                this.Close();
+
+        }
+
+        private void linkLabelMiPerfil_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            abrirmihija(new Precentacion.FormUserPerfil());
+        }
+    }
+}
---
+++ Precentacion/FormReoverPassword.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Precentacion
+{
+    partial class FormReoverPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtUserRequesting = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your User Name or Email";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(84, 154);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(82, 20);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // txtUserRequesting
+            // 
+            this.txtUserRequesting.Location = new System.Drawing.Point(88, 76);
+            this.txtUserRequesting.Name = "txtUserRequesting";
+            this.txtUserRequesting.Size = new System.Drawing.Size(364, 26);
+            this.txtUserRequesting.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(352, 108);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 40);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // FormReoverPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 250);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUserRequesting);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Name = "FormReoverPassword";
+            this.Text = "FormReoverPassword";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtUserRequesting;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
